895d21d1d72054fc48866e3db6fbe0140f632186
r-i-c-h <rich@richwerden.com>
2019-01-02 02:35:25 -0800

    Base Auth/Login/Logout/Validation Complete

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,12 @@ const router = express.Router();
 
 const User = require('../models/User');
 
+/*** User Registration ***/
 router.get('/register', (req, res) => res.render('register'));
 // Register Handler
 router.post('/register', (req, res) => {
-  const { name, email, password, password2 } = req.body;
-
   let errors = [];
+  const { name, email, password, password2 } = req.body;
   // Mini Validation for all fields
   if (!name || !email || !password || !password2) {
     let errMsg = 'You forgot something. Please fill in all fields';
@@ -67,6 +67,11 @@ router.post('/register', (req, res) => {
 });
 
 router.get('/login', (req, res) => res.render('login'));
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('sucess_msg','You have been logged out');
+  res.redirect('/users/login');
+});
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/dashboard',
@@ -74,9 +79,6 @@ router.post('/login', (req, res, next) => {
     failureFlash: true
   })(req, res, next);
 });
-
-
-
 
 
 module.exports = router;
